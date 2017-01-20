065ab5dafd8d2e2c857fb0996f10ea090e446cfd
Fran Goldner <fran.goldner@gmail.com>
2017-01-20 20:06:32 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Temperature Controller");
+
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+
+
+
+
+
+
+}
+
+
---
+++ src/sample/MainController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+
+    private ToggleGroup toggleGroupHeating = new ToggleGroup();
+
+    public MainController(){
+    }
+
+    @FXML
+    private void initialize(){
+
+
+
+    }
+
+
+
+}
